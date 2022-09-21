a7b99a3fbb19556bc963da2ee2743f5998b481c0
Stinkin <stinkinevil@hotmail.com>
2022-09-20 21:36:29 -0300

    Clase

---
+++ JavaScript/js/variables.js
@@ -0,0 +1,37 @@
+// variables a-z, 1-9,$,_
+
+nombre="Lola";
+console.log(nombre)
+console.log(typeof nombre)
+edad = 23;
+console.log(edad)
+console.log(typeof edad)
+
+var  numero;
+console.log(numero);
+numero=100;
+console.log(numero);
+
+var numero2=3;
+console.log(numero2);
+// +- * ** / % 
+
+var div=numero/numero2;
+console.log(div);
+var resto=numero % numero2;
+console.log(resto);
+
+const IVA=21.0;
+console.log(IVA);
+
+
+var nuevoValor=new Number(23);
+console.log(nuevoValor);
+
+var ejemplo1 ="890";
+console.log(ejemplo1)
+var conversion=Number.parseInt(ejemplo1);
+console.log(typeof conversion)
+
+var nombre2 = prompt("Ingrese su nombre ");
+document.write("<H1> Hola, ", nombre2,"</H1>");
