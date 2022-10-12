b847da8b3b9e3afa973e5ab85a0b1c0205d7faed
Stinkin <stinkinevil@hotmail.com>
2022-10-11 21:19:12 -0300

    Fin de la clase

--- DOM/mapMet.js
+++ DOM/mapMet.js
@@ -1,44 +1,39 @@
-const persona=[{
-    id:0,
-    nombre:"Lola",
-    apellido:"Mora",
-    edad:25
-},
-{
-    id:1,
-    nombre:"Juan",
-    apellido:"Perez",
-    edad:45
-},
-{
-    id:2,
-    nombre:"Mara",
-    apellido:"Lopez",
-    edad:33
-},
-{
-    id:3,
-    nombre:"Lucas",
-    apellido:"Ramos",
-    edad:55
-},
-]
+const persona = [
+  {
+    id: 0,
+    nombre: "Lola",
+    apellido: "Mora",
+    edad: 25,
+  },
+  {
+    id: 1,
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 45,
+  },
+  {
+    id: 2,
+    nombre: "Mara",
+    apellido: "Lopez",
+    edad: 33,
+  },
+  {
+    id: 3,
+    nombre: "Lucas",
+    apellido: "Ramos",
+    edad: 55,
+  },
+];
 
-
-
-
-
-persona.map(elemento=> {
-    //renderiza codigo html 
-    document.body.innerHTML +=
-    `<ul>
+persona.map((elemento) => {
+  //renderiza codigo html
+  document.body.innerHTML += `<ul>
     <li>
     Id:${elemento.id} <br>
-
     Nombre:${elemento.nombre}<br>
     Apellido:${elemento.apellido}<br>
     Edad:${elemento.edad}<br>
     </li>
     </ul>
-    `
-})
+    `;
+});
